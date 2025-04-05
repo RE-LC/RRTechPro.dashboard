c90d46b2d36b9f3f261b1e59082a8f0bc656299a
RE-LC <rajesh.epili.code@gmail.com>
2025-04-05 14:39:33 +0200

    Update categories/page.tsx

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,6 +1,7 @@
 import { DataTable } from "@/components/data-table/data-table";
 import { columns } from "@/columns/category.column";
 import { prisma } from "@/lib/prisma";
+import { Category } from "@prisma/client";
 
 export default async function Posts() {
     const categories = await prisma.category.findMany({
@@ -16,8 +17,8 @@ export default async function Posts() {
         },
     });
 
-    const formattedCategories = categories.map(category => ({
-        ...category,
+    const formattedCategories = categories.map(item: Category => ({
+        ...item,
         posts: category._count.posts
     }));
 
