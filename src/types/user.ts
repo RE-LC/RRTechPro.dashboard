95f1b8af68087ddc7218ef7390b560dda9747c43
Rajesh Epili <119894953+Linux-RE@users.noreply.github.com>
2025-04-06 15:49:47 +0200

    Integrated clerk components and user data to sidebar

--- src/types/user.ts
+++
@@ -1,9 +0,0 @@
-interface User {
-    id: string;
-    fullName: string;
-    firstName: string;
-    lastName: string;
-    username: string;
-    primaryEmailAddressId: string;
-    imageUrl?: string;
-}
